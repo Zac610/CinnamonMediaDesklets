b91709e26231f571c2c5a419271d8a45948c683c
Zac610 <ramas_rtl@hotmail.com>
2017-01-08 22:43:46 +0100

    used xdg picture dir as default images directory

--- FamilyPortrait@zac/desklet.js
+++ FamilyPortrait@zac/desklet.js
@@ -2,6 +2,8 @@ const Desklet = imports.ui.desklet;
 const St = imports.gi.St;
 const Clutter = imports.gi.Clutter;
 const Util = imports.misc.util;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 
 const DEFAULT_WIDTH = 100;
 
@@ -10,10 +12,36 @@ function VideoDesk(metadata, desklet_id)
 	this._init(metadata, desklet_id);
 }
 
-function getDefaulImagesDir()
-{
-	return "/home/zac/Documenti/Immagini/"
+
+function getUserImagesDir() { // from fileUtils.js
+    // Didn't find a function returning the user dirs, so parsing the user-dirs.dirs file to get it
+    let userdirsFile = Gio.file_new_for_path(GLib.get_home_dir()+"/.config/user-dirs.dirs");
+    let path;
+    if (userdirsFile.query_exists(null)){
+        try{
+            let data = userdirsFile.load_contents(null);
+            let dataDic = new Array();
+            let lines = data[1].toString().split("\n");
+            for (var i in lines){
+                if (lines[i][0]=="#") continue;
+                let line = lines[i].split("=", 2);
+                if (line.length==2){
+                    dataDic[line[0]] = line[1];
+                }
+            }
+            if (dataDic["XDG_PICTURES_DIR"])
+                path = dataDic["XDG_PICTURES_DIR"].substring(1, dataDic["XDG_PICTURES_DIR"].length-1).replace("$HOME", GLib.get_home_dir());
+            else
+                path = GLib.get_home_dir() + '/Images';
+        }catch(e){
+            path = GLib.get_home_dir() + '/Images';
+        }
+    }else path = GLib.get_home_dir() + '/Images';
+    let file = Gio.file_new_for_path(path);
+    if (file.query_exists(null)) return path;
+    else return null;
 }
+
 
 VideoDesk.prototype =
 {
@@ -25,7 +53,7 @@ VideoDesk.prototype =
 
 		this.window = new St.Bin();
 
-		let imgFilename = getDefaulImagesDir() + 'image.jpg';
+		let imgFilename = getUserImagesDir() + '/image.jpg';
 		this._clutterTexture = new Clutter.Texture({keep_aspect_ratio: true});
 		this._clutterTexture.set_from_file(imgFilename)
 
@@ -35,11 +63,19 @@ VideoDesk.prototype =
 		//~ this._clutterBox.set_width(this.metadata["width"]);
 		this._clutterBox.set_width(DEFAULT_WIDTH);
 		this._clutterBox.add_actor(this._clutterTexture);
+
+		// Aggiunge un testo se serve
+		this.text = new St.Label();
+		this.text.set_text("desklet id: "+ desklet_id);
+		this._clutterBox.add_actor(this.text);
+
 		this.window.add_actor(this._clutterBox);
+		//~ global.set_user_resizable(true);
 
 		this.setContent(this.window);
 	}
 }
+
 
 function main(metadata, desklet_id)
 {
